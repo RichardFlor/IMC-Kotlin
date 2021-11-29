a3c2529e9e4ea450f8c1587e44676a5c48143445
RICHARD MATHEUS FLOR DOROTEIO <21194072@CFP127ALUNO.LOCAL>
2021-11-29 15:34:12 -0300

    Login funcionando

--- app/src/main/java/com/example/imcapp/CadastroUsuarioActivity.kt
+++ app/src/main/java/com/example/imcapp/CadastroUsuarioActivity.kt
@@ -5,16 +5,32 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Toast
 
 class CadastroUsuarioActivity : AppCompatActivity() {
 
     lateinit var editEmail: EditText
+    lateinit var editSenha: EditText
+    lateinit var editNome: EditText
+    lateinit var editAltura: EditText
+    lateinit var editProfissao: EditText
+    lateinit var editDataNascimento: EditText
+    lateinit var radioFeminino: RadioButton
+    lateinit var radioMasculino: RadioButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
 
         editEmail = findViewById(R.id.cadastro_usuario_email)
+        editSenha = findViewById(R.id.cadastro_usuario_senha)
+        editNome = findViewById(R.id.cadastro_usuario_nome)
+        editProfissao = findViewById(R.id.cadastro_usuario_profissao)
+        editDataNascimento = findViewById(R.id.cadastro_usuario_nascimento)
+        editAltura = findViewById(R.id.cadastro_usuario_altura)
+        radioFeminino = findViewById(R.id.radio_femino)
+        radioMasculino = findViewById(R.id.radio_masculino)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -30,8 +46,18 @@ class CadastroUsuarioActivity : AppCompatActivity() {
         //cria um editor para o arquivo
         val editor = dados.edit()
         editor.putString("email", editEmail.text.toString())
+        editor.putString("senha", editSenha.text.toString())
+        editor.putString("nome", editNome.text.toString())
+        editor.putFloat("altura", editAltura.text.toString().toFloat())
+        editor.putString("nascimento", editDataNascimento.text.toString())
+        editor.putString("profissao", editProfissao.text.toString())
+        editor.putString("sexo", if(radioFeminino.isChecked) "F" else "M")
+
+
         editor.apply()
 
+        Toast.makeText(this, "Usuário cadastrado com sucesso!!", Toast.LENGTH_SHORT).show()
+        finish()
 
 
         return true
--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -3,17 +3,35 @@ package com.example.imcapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editEmail: EditText
+    lateinit var editSenha: EditText
+    lateinit var btnEntrar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Remover toolbar
         supportActionBar?.hide()
 
+        //Instanciar o botão entrar
         val botaoCriarConta = findViewById<View>(R.id.botao_criar_conta)
+
+        editEmail = findViewById(R.id.edit_login_email)
+        editSenha = findViewById(R.id.edit_login_senha)
+        btnEntrar = findViewById(R.id.btn_entrar)
+
+        btnEntrar.setOnClickListener {
+            login()
+        }
 
         //cria um ouvidor
         botaoCriarConta.setOnClickListener{
@@ -23,5 +41,26 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    private fun login() {
+
+        val arquivo = getSharedPreferences("cadastro", MODE_PRIVATE)
+
+        val emailDigitado = editEmail.text.toString()
+        val senhaDigitada = editSenha.text.toString()
+
+        val email = arquivo.getString("email", "")
+        val senha = arquivo.getString("senha", "")
+
+
+
+        if (emailDigitado == email && senhaDigitada == senha){
+            val intent = Intent(this, DashboardActivity::class.java)
+        startActivity(intent)
+        }
+        else{
+        Toast.makeText(this, "Usuario ou senha incorretos!", Toast.LENGTH_SHORT).show()
+       }
     }
 }
